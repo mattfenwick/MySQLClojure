1b910b4d51eabbd93d9aa97cf52d52114b0a456a
Matt Fenwick <mfenwick100@gmail.com>
2014-09-22 12:40:26 -0400

    more junk

---
+++ queries.sql
@@ -0,0 +1,5 @@
+select count(*) from largetable;
+
+select count(*) from largetable where id > 25000000;
+
+select count(*) from largetable where id <= 25000000;
---
+++ test/clojql/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojql.test.core
+  (:use [clojql.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
